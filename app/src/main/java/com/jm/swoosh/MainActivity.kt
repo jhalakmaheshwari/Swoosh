3a7530b47b55059ecb50b5fb64f5203e68f73b5d
jmaheshwari <Jhalak_Maheshwari@intuit.com>
2020-05-13 15:14:05 -0700

    adding intent to go from one activity to another

--- app/src/main/java/com/jm/swoosh/MainActivity.kt
+++ app/src/main/java/com/jm/swoosh/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.jm.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Intent is to communicate between activities
+        getStartedBtn.setOnClickListener {
+            // explicit Intent
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
